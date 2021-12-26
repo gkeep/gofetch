9ed0cbe858a57f2aab45554717bab7993129fb31
jturn <34032205+Jturnxd@users.noreply.github.com>
2021-12-26 18:12:57 +0300

    Move fetch functions to separate file (#1)
    * move fetch functions to seperate file
    * add fetch.go to git

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
-	"os/user"
 	"strings"
 
-	"github.com/klauspost/cpuid"
 	"gopkg.in/yaml.v2"
 )
 
@@ -49,87 +45,6 @@ func load_config() Config {
 	}
 
 	return cfg
-}
-
-func get_hostname() string {
-	hostname, err := os.Hostname()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return string(hostname)
-}
-
-func get_username() string {
-	username, err := user.Current()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return string(username.Username)
-}
-
-func get_desktop_env() string {
-	de := os.Getenv("XDG_CURRENT_DESKTOP")
-
-	if de == "" {
-		return "None"
-	}
-
-	return de
-}
-
-func get_cpu(clrs Colors) (string, Colors) {
-	cpu_name := cpuid.CPU.BrandName
-	cpu_name = strings.Replace(cpu_name, "(R)", "", -1)
-	cpu_name = strings.Replace(cpu_name, "(TM)", "", -1)
-
-	switch cpuid.CPU.VendorString {
-	case "GenuineIntel":
-		clrs.cpu = "\033[34m"
-	case "AuthenticAMD":
-		clrs.cpu = "\033[31m"
-	default:
-		clrs.cpu = "\033[0m"
-	}
-
-	return fmt.Sprint(cpu_name), clrs
-}
-
-func get_distro() string {
-	file, err := os.Open("/etc/os-release")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	var distro string
-
-	for scanner.Scan() {
-		text := scanner.Text()
-
-		if strings.HasPrefix(text, "ID=") {
-			distro = text[3:]
-			break
-		}
-	}
-
-	return distro
-}
-
-func get_uptime() string {
-	command := exec.Command("/usr/bin/uptime", "-p")
-
-	uptime, err := command.Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return strings.Replace(string(uptime), "\n", "", -1)
 }
 
 func get_colors(distro string) Colors {
@@ -195,5 +110,4 @@ func main() {
 		fmt.Printf("%s %s %s\n", color_print(colors.secondary, info_list[i]),
 			config.Separator, template)
 	}
-
 }
